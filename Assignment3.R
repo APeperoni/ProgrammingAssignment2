216c6da86f164c5b4593f4d17400778a4c37fa65
APeperoni <76793005+APeperoni@users.noreply.github.com>
2020-12-30 17:57:21 -0300

    Add files via upload

---
+++ Assignment3.R
@@ -0,0 +1,27 @@
+makeCacheMatrix <- function(x = matrix()) {
+  invrs <- NULL
+  set <- function(y) {
+    x <- y
+    invrs <- NULL
+  }
+  get <- function() x {
+  setinverse <- function(inverse) invrs <- inverse
+  getinverse <- function() invrs
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)}
+  }
+## Creates a reversible matrix
+
+cacheSolve <- function(x, ...) {
+  invrs <= x$getinverse()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  mat <- x$get()
+  invrs <- solve(mat, ...)
+  x$setinverse(invrs)
+  invrs
+}
+    ## Returns a matrix that is the inverse of 'x'
